d0490bb06d7112c1f2e5182083215928aa050414
joh2302120 <joh2302120@maricopa.edu>
2023-11-30 21:40:09 -0700

    color change button

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+function changeColor() {
+    let color = document.body.style.color;
+       if (color = "D6FFE4") {
+          document.body.style.background = "#D6FFE4";
+          document.body.style.text="#29001B";
+        } else {
+           document.body.style.background = "#29001B";
+           document.body.style.text="#D6FFE4";
+         }
+      }

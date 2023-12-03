e177d1a5c029f9020194d089d00824c38755c779
joh2302120 <joh2302120@maricopa.edu>
2023-12-02 22:08:16 -0700

    added java for light dark button

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,17 @@
-function changeColor() {
-    let color = document.body.style.color;
-       if (color = "D6FFE4") {
-          document.body.style.background = "#D6FFE4";
-          document.body.style.text="#29001B";
-        } else {
-           document.body.style.background = "#29001B";
-           document.body.style.text="#D6FFE4";
-         }
-      }
+const darkMode = document.getElementById("darkBtn");
+const lightMode = document.getElementById("lightBtn");
+
+darkMode.addEventListener('click', function onClick() {
+  document.body.style.backgroundColor = "#29001B";
+  document.body.style.color = "#D6FFE4";
+  darkMode.style.backgroundColor = "#29001B";
+  darkMode.style.borderColor = "#D6FFE4";
+  lightMode.style.borderColor = "#D6FFE4";
+  darkMode.style.color = "#D6FFE4";
+});
+
+lightMode.addEventListener('click', function onClick() {
+  document.body.style.backgroundColor = "#D6FFE4";
+  document.body.style.color = "#29001B";
+  lightMode.style.borderColor = "#29001B";
+});
